e0c2abf5b9acd649d7d3b68f88537f1d64db1ac9
aztech-adrian <ahernandez@equipovision.com>
2021-05-27 18:56:41 -0700

    nav bar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,18 +1,25 @@
 <template>
   <v-list>
-    <v-list-item v-for="([icon, text], i) in items" :key="i" link>
-      <router-link to="/" style="display: inherit;">
-        <v-list-item-icon>
-          <v-icon>{{ icon }}</v-icon>
-        </v-list-item-icon>
+    <v-list-item v-for="(item, i) in items" :key="i" link :to="item.path">
+      <v-list-item-icon>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ text }}</v-list-item-title>
-        </v-list-item-content>
-      </router-link>
+      <v-list-item-content>
+        <v-list-item-title>{{ item.text }}</v-list-item-title>
+      </v-list-item-content>
     </v-list-item>
+    <!-- <v-list-item v-for="([icon, text, path], i) in admin_items" :key="text" :to="path" link>
+      <v-list-item-icon>
+        <v-icon>{{ icon }}</v-icon>
+      </v-list-item-icon>
 
-    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>{{ text }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item> -->
+
+    <v-list-item @click="logout()">
       <v-list-item-icon>
         <v-icon>mdi-logout</v-icon>
       </v-list-item-icon>
@@ -29,16 +36,32 @@ export default {
   data: () => ({
     open: false,
     isAdmin: false,
-    items: [["mdi-account-circle-outline", "Home"]],
+    items: [
+      {
+        icon: "mdi-account-circle-outline",
+        text: "Home",
+        path: "/",
+        admin: false,
+      },
+      {
+        icon: "mdi-account-group",
+        text: "Members",
+        path: "/members",
+        admin: true,
+      },
+    ],
+    // admin_items: [["mdi-account-group", "Members", "/members"]],
   }),
-  methods: {},
+  methods: {
+    logout() {
+      document.cookie = "session" + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      this.$router.push({ path: "/login" });
+    },
+  },
   created() {
     if (localStorage.admin) {
       this.isAdmin = true;
     }
-  },
-  logout(){
-     document.cookie = 'session' +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   },
   mounted() {},
 };
